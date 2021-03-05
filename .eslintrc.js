2150c1e53098b2bc35e5f846a133fd59b7eaf24a
P-Jeremy <jemyplu@gmail.com>
2021-03-05 13:18:28 +0100

    Config eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   env: {
-    commonjs: true,
     es6: true,
     node: true,
     jest: true,
@@ -16,5 +15,9 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
+    'no-new': 0,
+    'no-underscore-dangle': 0,
+    'class-methods-use-this': 0,
+    'max-classes-per-file': 0,
   },
 };
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # kata_todo_class
+
+there are two test files in the src folder
+
+Implement the code in order to pass the tests
